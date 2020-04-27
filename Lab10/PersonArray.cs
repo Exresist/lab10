83c337e4a7a36c22b85300120623e60027b6c4ed
Herman <gpahomov@inbox.ru>
2020-04-27 13:26:08 +0500

    Lab10

---
+++ Lab10/PersonArray.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+
+namespace Lab10
+{
+    public class PersonArray
+    {
+        public Person[] Arr;
+        public int Count;
+
+        // Конструктор класса без параметров
+        public PersonArray()
+        {
+            Arr = null;
+            Count = 0;
+        }
+
+        // Конструктор класса с параметрами
+        public PersonArray(int size)
+        {
+            Arr = new Person[size];
+            Count = size;
+        }
+
+        // Индексация к элементу масива 
+        public Person this[int index]
+        {
+            get => Arr[index];
+            set => Arr[index] = value;
+        }
+
+        // Ввод размера массива
+        public static int InputSize()
+        {
+            int number;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Введите рамер массива:");
+            Console.ForegroundColor = ConsoleColor.White;
+            while (!int.TryParse(Console.ReadLine(), out number) || number < 0 || number > 100)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка. Вы неверно ввели размер массива. Повторите ввод.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            return number;
+        }
+
+        // Вывод всей инфорамции о персонах, информация о которых хранится в PersonArray
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Персоны:\n");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            for (int i = 0; i < Count; i++)
+            {
+                this[i].Show();
+                Console.WriteLine();
+            }
+        }
+
+        public void Sort()
+        {
+            Array.Sort(Arr, new SortByAge());
+        }
+
+        public int Search(int age)
+        {
+            try
+            {
+                return Array.BinarySearch(Arr, age, new SortByAge());
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+
+        public int Search(string name)
+        {
+            try
+            {
+                return Array.BinarySearch(Arr, name, new SortByName());
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+
+        public void RandomGeneration(int size)
+        {
+            Random random = new Random();
+            Person[] people = new Person[size];
+            for (int i = 0; i < size; i++)
+            {
+                people[i] = random.Next(1, 3) switch
+                {
+                    1 => new Administration(),
+                    2 => new Engineer(),
+                    3 => new Worker(),
+                    _ => people[i]
+                };
+            }
+
+            Arr = people;
+            Count = size;
+        }
+    }
+}
---
+++ Lab10/TypeSortnig.cs
@@ -0,0 +1,98 @@
+﻿﻿using System;
+ using System.Collections;
+ using System.Collections.Generic;
+using System.Text;
+
+ namespace Lab10
+{
+    // Сортировка массива Person по имени
+    public class SortByName : IComparer
+    {
+        int IComparer.Compare(object obj1, object obj2)
+        {
+            Person p1 = (Person)obj1;
+            Person p2 = (Person)obj2;
+            return String.Compare(p1.Name, p2.Name);
+        }
+    }
+
+    // Сортировка массива Person по фамилии
+    public class SortBySurname : IComparer
+    {
+        int IComparer.Compare(object obj1, object obj2)
+        {
+            Person p1 = (Person)obj1;
+            Person p2 = (Person)obj2;
+            return String.Compare(p1.Surname, p2.Surname);
+        }
+    }
+
+    // Сортировка массива Person по полу
+    public class SortByGender : IComparer
+    {
+        int IComparer.Compare(object obj1, object obj2)
+        {
+            Person p1 = (Person)obj1;
+            Person p2 = (Person)obj2;
+            return String.Compare(p1.Gender, p2.Gender);
+        }
+    }
+
+    // Сортировка массива Person по возрасту
+    public  class SortByAge : IComparer
+    {
+        int IComparer.Compare(object obj1, object obj2)
+        {
+            Person p1 = obj1 as Person;
+            Person p2 = obj2 as Person;
+            int p1Age = p1.Age();
+            int p2Age = p2.Age();
+            if (p1Age > p2Age)
+                return 1;
+            if (p1Age < p2Age)
+                return -1;
+            return 0;
+        }
+    }
+
+    // Сортировка массива Person по дате рождения
+    public class SortByDOB : IComparer
+    {
+        int IComparer.Compare(object obj1, object obj2)
+        {
+            Person p1 = obj1 as Person;
+            Person p2 = obj2 as Person;
+            if (p1.DateOfBirth.Year > p2.DateOfBirth.Year)
+            {
+                return 1;
+            }
+
+            if (p1.DateOfBirth.Month < p2.DateOfBirth.Month)
+            {
+                return 1;
+            }
+
+            if (p1.DateOfBirth.Day < p2.DateOfBirth.Day)
+            {
+                return 1;
+            }
+
+            return -1;
+        }
+    }
+
+    // Сортировка массива Person по стажу
+    public class SortByExperience : IComparer
+    {
+        int IComparer.Compare(object obj1, object obj2)
+        {
+            Person p1 = obj1 as Person;
+            Person p2 = obj2 as Person;
+            if (p1.Experience > p2.Experience)
+                return 1;
+            if (p1.Experience < p2.Experience)
+                return -1;
+            return 0;
+        }
+    }
+}
